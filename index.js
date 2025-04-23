29a4426a337b14860794ecafb863aabbd0850153
Jai Prakash Rai <jaiprakashrai733@gmail.com>
2025-04-23 19:37:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello AGainnnnnnn')
+  res.send('Korbo Lorbo JEetbooooo')
 })
 
 app.get('/courses', function (req, res) {
